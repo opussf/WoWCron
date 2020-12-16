7a7f5f62591fd758e7d86cc13b033dac2969bd40
opussf <github@zz9-za.com>
2020-12-16 11:14:57 -0800

    Removing coroutines

--- src/wowcron.lua
+++ src/wowcron.lua
@@ -49,13 +49,14 @@ wowCron.toRun = {}
 function wowCron.OnLoad()
 	SLASH_CRON1 = "/CRON"
 	SlashCmdList["CRON"] = function(msg) wowCron.Command(msg); end
-	wowCron_Frame:RegisterEvent("ADDON_LOADED")
-	wowCron_Frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	wowCron_Frame:RegisterEvent( "ADDON_LOADED" )
+	wowCron_Frame:RegisterEvent( "PLAYER_ENTERING_WORLD" )
+	wowCron_Frame:RegisterEvent( "PLAYER_ALIVE" )
 end
 function wowCron.OnUpdate()
-	-- if the toRun list has items, this should not be dead.
-	if coroutine.status(wowCron.processThread) ~= "dead" then
-		coroutine.resume(wowCron.processThread)
+	-- if there are still events in the queue to process
+	if( #wowCron.toRun > 0 ) then
+		wowCron.RunNowList()
 	end
 	local nowTS = time()
 	if (wowCron.lastUpdated < nowTS) and (nowTS % 60 == 0) then
@@ -70,7 +71,6 @@ function wowCron.ADDON_LOADED()
 	wowCron.lastUpdated = time()
 	wowCron.ParseAll()
 	wowCron.BuildSlashCommands()
-	wowCron.processThread = coroutine.create(wowCron.RunNowList)
 	--wowCron.Print("Loaded")
 end
 function wowCron.PLAYER_ENTERING_WORLD()
@@ -79,6 +79,9 @@ function wowCron.PLAYER_ENTERING_WORLD()
 	wowCron.BuildSlashCommands()
 	wowCron.started = time()
 	wowCron.macros["@first"] = wowCron.BuildFirstCronMacro()
+end
+function wowCron.PLAYER_ALIVE()
+	print( "PLAYER_ALIVE!" )
 end
 -- Support Code
 function wowCron.BuildFirstCronMacro()
@@ -95,10 +98,10 @@ function wowCron.BuildRunNowList()
 			table.insert( wowCron.toRun, cmd )
 		end
 	end
-	wowCron.processThread = coroutine.create(wowCron.RunNowList)
 end
 function wowCron.RunNowList()
-	while (#wowCron.toRun > 0) do
+	-- run a single item from the list per update
+	if (#wowCron.toRun > 0) then
 		cmd = table.remove( wowCron.toRun, 1 )
 		--print("CMD: "..(cmd or "nil"))
 		if cmd then
@@ -110,7 +113,6 @@ function wowCron.RunNowList()
 				isGood = isGood or func( slash, parameters )
 			end
 		end
-		coroutine.yield()
 	end
 end
 -- Begin Handle commands
