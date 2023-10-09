208e8a2254fd3c869598ab52c5c3eaf08bafcba7
opussf <github@zz9-za.com>
2023-10-09 01:01:35 -0700

    AT feature
    Remember three months from this date. Good things are in store for you.

--- src/wowcron.lua
+++ src/wowcron.lua
@@ -452,3 +452,109 @@ function wowCron.Print( msg, showName)
 	end
 	DEFAULT_CHAT_FRAME:AddMessage( msg )
 end
+wowCron.AtCommandList = {
+	["help"] = {
+		["func"] = wowCron.PrintHelp,
+		["help"] = {"", "Print this help." },
+	},
+	["global"] = {
+		["func"] = function( msg ) wowCron.AtCommand( msg, true ); end,
+		["help"] = {"<commands>", "Sets global flag" },
+	},
+}
+function wowCron.AtCommand( msg, isGlobal )
+	wowCron.global = isGlobal
+	cmd, parameters = wowCron.DeconstructCmd( msg )
+	print( cmd, parameters )
+	cmd = string.lower( cmd )
+	local cmdFunc = wowCron.AtCommandList[cmd]
+	if cmdFunc then
+		cmdFunc.func( parameters )
+	else
+		wowCron.AtAddEntry( msg )
+	end
+end
+function wowCron.AtAddEntry( msg )
+	msg = string.lower( msg )
+	print( "AtAddEntry( "..msg.." )" )
+	local shortcuts = { ["noon"] = "12:00:00", ["midnight"] = "00:00:00", ["teatime"] = "16:00:00" }
+	local targetTime = date( "*t" )
+	msgItem, msg = strsplit( " ", msg, 2 )
+
+	while( msgItem and msg ) do -- only parse as part of the time string.  A command starts with a "/"
+		print( "-->"..msgItem.."<--:".. msg..":" )
+		-- find a date.  Do this first because the time string is 'funky'
+		_, _, month, split, day, year = strfind( msgItem, "([%d]?%d)([/.-])([%d%d]+)[/]?([%d%d]*)" )
+		-- date of the form MMDD[CC]YY, MM/DD/[CC]YY, DD.MM.[CC]YY or [CC]YY-MM-DD.
+
+		if( split == "." ) then
+			d = day
+			day = month
+			month = d
+		end
+		if( split == "-" ) then
+			y = year
+			year = month
+			month = day
+			day = y
+		end
+		print( ( month or "nil").."/"..(day or "nil").."/"..(year or "nil").." split:"..( split or "nil" ) )
+		if( split ) then  -- having split says this is a date.
+			targetTime.month = month
+			targetTime.day = day
+			if( tonumber( year ) < 100 ) then
+				century = math.floor( targetTime.year / 100 ) * 100
+				year = century + year
+			end
+			targetTime.year = year
+		end
+
+
+		-- find and replace 'shortcut' string
+		msgItem = shortcuts[msgItem] or msgItem
+
+		--break
+
+
+		-- find a time string
+		if( tonumber( msgItem ) and tonumber( msgItem ) < 1000 ) then -- if the item is a number less than 1000, add a "0" to the start.
+			msgItem = "0"..msgItem
+		end
+		_, _, hourIn, minIn, secIn = strfind( msgItem, "([%d]?%d)[:]?([%d%d]+)[:]?([%d%d]*)" )
+		print( ( hourIn or "nil")..":"..( minIn or "nil" )..":"..( secIn or "nil" ) )
+		if( hourIn ) then
+			targetTime.hour = tonumber( hourIn )
+		end
+		if( minIn ) then
+			targetTime.min = tonumber( minIn )
+		end
+		if( secIn ) then
+			targetTime.sec = tonumber( secIn ) or 0
+		end
+
+		-- find AM/PM and modify the time
+		_, _, AMPM = strfind( msgItem, "([ap]m)" )
+		if( AMPM and targetTime["hour"] <= 12 and AMPM == "pm" ) then
+			targetTime.hour = targetTime.hour + 12
+		end
+
+
+
+
+		print( date( "-->%x %X", time( targetTime ) ) )
+		msgItem, msg = strsplit( " ", msg, 2 )
+
+	end
+	print( "Final -->"..msgItem.."<--" )
+	targetTS = time( targetTime )
+	if( targetTS < time()) then
+		targetTS = targetTS + 86400
+		print( date( "-->%x %X", targetTS ) )
+	end
+	atTable = wowCron.global and at_global or at_player
+	atTable[targetTS] = atTable[targetTS] or {}
+	table.insert( atTable[targetTS], msgItem )
+	print( "now: "..time().." target: "..targetTS )
+
+
+end
