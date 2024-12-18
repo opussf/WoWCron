9d13d4bde13cf8626dea8dbaab458634f617f128
opussf <github@zz9-za.com>
2024-12-17 17:24:56 -0800

    Stubs update.  Only ParseAll on delete if actually deleting.
    If you don't run your own life, somebody else will.

--- test/wowTest.lua
+++ test/wowTest.lua
@@ -1,7 +1,7 @@
 -----------------------------------------
 -- Author  :  Opussf
--- Date    :  December 07 2024
--- Revision:  9.5-14-gb4ad3de
+-- Date    :  December 12 2024
+-- Revision:  9.5-15-gcefc9c2
 -----------------------------------------
 -- This is an uber simple unit test implementation
 -- It creates a dictionary called test.
