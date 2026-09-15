30970c4d211714086a4747451ece2cfed74b5f41
opussf <github@zz9-za.com>
2026-09-15 15:23:26 -0700

    @rcheck and @rdone macros for READY_CHECK and READY_CHECK_FINSIHED
    <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
    <html><head>
    <title>404 Not Found</title>
    </head><body>
    <h1>Not Found</h1>
    <p>The requested URL was not found on this server.</p>
    </body></html>

--- src/wowcron.lua
+++ src/wowcron.lua
@@ -51,7 +51,8 @@ wowCron.macros = {  -- keep a 1 to 1 mapping for macro to event.
 	["@xp"]       = { ["event"] = "PLAYER_XP_UPDATE" },
 	["@ilvl"]     = { ["event"] = "PLAYER_AVG_ITEM_LEVEL_UPDATE" },
 	["@achv"]     = { ["event"] = "ACHIEVEMENT_EARNED" },
-	["@ready"]    = { ["event"] = "READY_CHECK" },
+	["@rcheck"]   = { ["event"] = "READY_CHECK" },
+	["@rdone"]    = { ["event"] = "READY_CHECK_FINISHED" },
 }
 wowCron.chatChannels = {
 	["/s"]    = "SAY",
